19a6dbdd919fc1010061223d95144067acf57f53
Raphi W <ditshej@gmail.com>
2023-06-09 15:49:54 +0200

    extract workspace id for testing into phpunit.xml

--- src/AJT/Toggl/tests/Toggl/APITests/V9/TogglAPIV9TestCase.php
+++ src/AJT/Toggl/tests/Toggl/APITests/V9/TogglAPIV9TestCase.php
@@ -8,7 +8,8 @@ use PHPUnit\Framework\TestCase;
 /**
  * Set up the following file to run this test:
  * \phpunit.xml by copying \phpunit-dist.xml and fill in the variables
- *  - toggl_api_key: See https://www.toggl.com/public/api#api_token for more information on the api key
+ * - toggl_api_key: See https://www.toggl.com/public/api#api_token for more information on the api key
+ * - toggl_test_workspace_id: Print it by running this test with XDebug (or similar) and have a look at the end of the setUp() method
  */
 class TogglAPIV9TestCase extends TestCase
 {
@@ -19,13 +20,16 @@ class TogglAPIV9TestCase extends TestCase
     {
         global $toggl_api_version;
         global $toggl_api_key;
+        global $toggl_test_workspace_id;
 
         $toggl_api_version = 'v9';
 
         $this->client = TogglClient::factory([
             'api_key' => $toggl_api_key,
         ]);
+        $this->workspace_id = $toggl_test_workspace_id;
+
+        // to detect your workspace id(s), print it/them here with XDebug (or similar)
         $workspaces = $this->client->getWorkspaces();
-        $this->workspace_id = $workspaces[0]['id'];
     }
 }
